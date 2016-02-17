55a8713e4dca08cb2304ed69064871f51a68196d
Nick Sorrell <nicksorrell@gmail.com>
2016-02-17 17:02:38 -0500

    Add more Gulp deps

--- gulpfile.js
+++ gulpfile.js
@@ -3,45 +3,35 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     cryptoJS = require('crypto-js'),
     fs = require("fs"),
+    del = require('del'),
+    minifyHTML = require('gulp-htmlmin'),
     SCO_data = require('./src/js/sco_data.json');
-    /*
-    minifycss = require('gulp-minify-css'),
-    minifyHTML = require('gulp-minify-html'),
-    imagemin = require('gulp-imagemin'),
-    del = require('del');
-    */
 
 gulp.task('js', function(){
-  return gulp.src(['src/js/main.js', 'src/js/SCORM_2004_APIWrapper.js'])
+  return gulp.src('src/js/main.js')
         .pipe(uglify())
-        //.pipe(concat('main.js'))
         .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('html-copy', function(){
-  return gulp.src('src/index.html')
-        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('crypto', function(){
   var rawStr = JSON.stringify(SCO_data);
   var wordArray = cryptoJS.enc.Utf8.parse(rawStr);
-  console.log(cryptoJS.enc.Base64.stringify(cryptoJS.SHA256(wordArray)));
   var base64 = cryptoJS.enc.Base64.stringify(wordArray);
+
   fs.writeFileSync('src/js/sco_data_enc.json', base64);
 });
 
 //Run this after the default task, and after you manually update the script tags to use the concat file
-/*gulp.task('html-min', function(){
-  return gulp.src('dist/*.html')
-        .pipe(minifyHTML())
+gulp.task('html-min', function(){
+  return gulp.src('src/index.html')
+        .pipe(minifyHTML({collapseWhitespace: true, minifyCSS: true}))
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function(){
   return del(['dist']);
-});*/
+});
 
 gulp.task('default', function(){
-  gulp.start('js', 'html-copy');
+  gulp.start('js', 'html-min');
 });
